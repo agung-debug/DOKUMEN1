894460d1ef2624424fb538f2fa6da085bc771928
Admin <triagung5053@gmail.com>
2025-10-27 09:34:08 +0700

    sin

---
+++ src/classNew.java
@@ -0,0 +1,25 @@
+public class classNew {// EXTRACT SUPERCLASS: Beverage menjadi induk untuk semua minuman
+    public abstract class Beverage {
+        protected String name;
+        protected double price;
+
+        // Constructor
+        public Beverage(String name, double price) {
+            this.name = name;
+            this.price = price;
+        }
+
+        // Encapsulate Field: Getter & Setter
+        public String getName() {
+            return name;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+
+        // Abstract Method (akan diimplementasi di Coffee)
+        public abstract void displayInfo();
+    }
+
+}
